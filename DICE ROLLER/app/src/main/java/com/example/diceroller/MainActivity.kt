a7dac7f444d1160123f2971bde95ebf60cb3fd83
TrangPC <nguyenthutrang542002@gmail.com>
2023-10-07 01:25:18 +0700

    first commit

---
+++ DICE ROLLER/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DICEROLLERTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+//            toast.show()
+//            val resultTextView: TextView = findViewById(R.id.textView)
+//            resultTextView.text = "6"
+            rollDice()
+        }
+        setContent {
+            DICEROLLERTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DICEROLLERTheme {
+        Greeting("Android")
+    }
+}
