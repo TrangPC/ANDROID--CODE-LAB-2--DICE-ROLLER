7abfebca0dadd34aab5afe16d4b40151ccfa8eb0
TrangPC <nguyenthutrang542002@gmail.com>
2023-10-07 21:28:40 +0700

    first commit

--- DICE ROLLER/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DICE ROLLER/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,15 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.diceroller.ui.theme.DICEROLLERTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,17 +17,17 @@ class MainActivity : ComponentActivity() {
 //            resultTextView.text = "6"
             rollDice()
         }
-        setContent {
-            DICEROLLERTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+//        setContent {
+//            DICEROLLERTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
     }
 
     private fun rollDice() {
@@ -52,18 +43,18 @@ class Dice(private val numSides: Int) {
         return (1..numSides).random()
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DICEROLLERTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DICEROLLERTheme {
+//        Greeting("Android")
+//    }
+//}
